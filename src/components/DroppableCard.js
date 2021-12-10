e74872ba7090407549f1a232a7b5caa62dce9ae1
Mr. Hands <mrhands31@gmail.com>
2021-12-10 13:00:57 +0100

    Mark card active when it can be dropped.

--- src/components/DroppableCard.js
+++ src/components/DroppableCard.js
@@ -1,16 +1,22 @@
 import { useDrop } from 'react-dnd';
 
 export default function DroppableCard(props) {
-	const [ collected, drop ] = useDrop(() => ({
+	const [ { isOver, canDrop }, drop ] = useDrop(() => ({
 		accept: 'card',
 		collect: monitor => ({
-			isover: (!!monitor.isOver()).toString()
+			isOver: monitor.isOver(),
+			canDrop: monitor.canDrop(),
 		}),
 		drop: card => props.onTaskDropped(props.owner, card),
 	}), [props.owner]);
 
+	let className = props.className;
+	if (isOver && canDrop) {
+		className += ' -active';
+	}
+
 	return (
-		<div ref={drop} {...collected} {...props}>
+		<div ref={drop} {...props} className={className}>
 			{props.children}
 		</div>
 	);
