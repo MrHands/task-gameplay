a2a22620b1d6b5b5d64e0af3875777e1089a18bc
Mr. Hands <mrhands31@gmail.com>
2022-03-29 17:43:48 +0200

    Improve opening page.

--- src/states/GameStart.js
+++ src/states/GameStart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 import './GameStart.scss';
 
@@ -11,8 +12,18 @@ export default class GameStart extends React.Component {
 
 		return (
 			<article className='o-gameStart'>
-				<button onClick={onStartGame}>Start Game</button>
-				<button onClick={onJumpToNight}>Jump Straight To Night Shift</button>
+				<div class="o-gameStart__explain">
+					<ReactMarkdown>
+{`# Up There They Love - Gameplay Prototype
+
+Due to a series of _hilarious_ mishaps that would take too long to get into here,
+you have been put in charge of the space ship _Love Itself_.`}
+					</ReactMarkdown>
+				</div>
+				<div class="o-gameStart__interact">
+					<button onClick={onStartGame}>Start Game</button>
+					<button onClick={onJumpToNight}>Skip To Night Shift</button>
+				</div>
 			</article>
 		);
 	}
