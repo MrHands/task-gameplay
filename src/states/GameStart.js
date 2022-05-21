5410c281fab2dc22d16fae172b3095201fcfe8dd
Mr. Hands <mrhands31@gmail.com>
2022-05-21 14:23:06 +0200

    Update README.

--- src/states/GameStart.js
+++ src/states/GameStart.js
@@ -18,25 +18,35 @@ export default class GameStart extends React.Component {
 
 Due to a series of _hilarious_ mishaps that would take too long to get into here,
 you have been put in charge of the space ship _Love Itself_. This vessel has a prototype
-engine installed, called the **S.E.X. drive**, and is powered by **sexual energy**, or _sexergy_.
+engine installed, called the _S.E.X. drive_, which is powered by sexual energy, or **sexergy**.
 
-As the **captain**, it is your responsibility to **manage** your **crew members** by assigning **tasks**
-to them during their shifts. You need to generate as much **sexergy** as possible so the ship can
-keep flying!
+Your goal as the male **Captain** of the ship is to collect as much sexergy as possible from
+your three female **Crew Members** to keep the S.E.X. drive purring and the ship sailing through
+space.
 
 ## Day shift
 
-During the _Morning_, _Afternoon_, and _Evening_ shifts, you assign each crew member to a task.
-The tasks have a **difficulty** and the crew member rolls a 1d20 to determine if they succeed, fail,
-or have a critical success. You can **bet crew stamina** to improve your odds. But be careful!
-Characters without any stamina remaining can only perform a _Rest_ task to restore it.
+During the _Morning_, _Afternoon_, and _Evening_ shifts, your crew members will assign themselves
+to different locations on the ship. Each location has **Tasks** that a crew member can perform, which
+cost **Stamina**. Some tasks will give them **Arousal**, which will help you during the _Night_ shift.
+
+All tasks for your crew members must be started by you. You can either spend their stamina or you
+can use your **Action Dice** to lower the amount of stamina required. If a task reaches 0 stamina
+required, it is started straight away. You can assign more than one Action Dice to a task. 
+
+Once you've spent all of your Action Dice, you can advance to the next shift.
+
+Finally, the **Rest** action is special: it restores stamina for the number of eyes on the
+Action Die that you assign. E.g. assigning a 4 will restore 4 stamina for the crew member.
 
 ## Night shift
 
-At the start of the _Night_, you pick a crew member to have sex with. You can then perform sex moves to
-generate **sexergy**.
+At the start of the night shift, you pick a crew member to have sex with. You can then perform
+sex moves to generate **sexergy**. The arousal you've built up during the day shifts is taken into
+this shift.
 
-When the crew member's lust bar reaches 200%, they orgasm. This doubles the sexergy you've generated so far.
+When the crew member's lust bar reaches 200% arousal, they have an orgasm. This doubles the sexergy
+you've generated so far.
 
 When the captain's lust bar reaches 100%, the night shift ends. The generated sexergy is added to the total.
 `}
